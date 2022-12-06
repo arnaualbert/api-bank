53c3fea47189d9f26af64fe72564b2d667771813
Arnau Albert <arnaualbert@pop-os.localdomain>
2022-12-06 18:03:10 +0100

    v3

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.post('/api/mod', (req, res) => {
 })
 
 
-// Listen on enviroment port or 5000
+
 app.listen(port, () => console.log(`Listening on port ${port}`))
 app.listen(3000, ()=>{
     console.log('Aquesta és la nostra API-REST que corre en http://localhost:3000')
